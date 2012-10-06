fad6f16c141a757e153dc0472dcb7d0d7a928ef2
william <william@10.0.1.6>
2012-10-06 10:44:36 -0700

    next time- keep maze on screen

--- MazeProject/src/mazeproject/MazeBuilder.java
+++ MazeProject/src/mazeproject/MazeBuilder.java
@@ -2,6 +2,7 @@
 package mazeproject;
 
 import java.awt.Color;
+import java.util.HashMap;
 import java.util.Random;
 
 public class MazeBuilder
@@ -13,6 +14,7 @@ public class MazeBuilder
     private MazePiece emptySpaceNW = new EmptySpace();
     private MazePiece currentPiece;
     private MazePiece adjacentPiece = new EmptySpace();
+    private HashMap<String, EmptySpace> mazePieceStore = new HashMap();
     private MazePiece randomPiece = new EmptySpace()
     {
         @Override
@@ -54,44 +56,83 @@ public class MazeBuilder
 
     public void randomBuild()
     {
+        int x = 0;
+        int y = 0;
         r = new Random();
         float r2;
         currentPiece = randomPiece;
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 638; i++)
         {
             r2 = r.nextFloat();
-            System.out.println(r2);
+            //System.out.println(r2);
             if (r2 <= .25)
             {
-
-                currentPiece.setNorthNeighbor(new EmptySpace());
+                y--;
+                currentPiece.setNorthNeighbor(nextSpace(x, y));
                 adjacentPiece = currentPiece.getNorthNeighbor();
+                System.out.println(adjacentPiece + "gotHere");
                 adjacentPiece.setSouthNeighbor(currentPiece);
             }
             if (r2 > .25 && r2 < .55)
             {
-
-                currentPiece.setEastNeighbor(new EmptySpace());
+                x++;
+                currentPiece.setEastNeighbor(nextSpace(x, y));
                 adjacentPiece = currentPiece.getEastNeighbor();
+                System.out.println(adjacentPiece + "gotHere");
                 adjacentPiece.setWestNeighbor(currentPiece);
             }
             if (r2 >= .55 && r2 <= .75)
             {
-
-                currentPiece.setSouthNeighbor(new EmptySpace());
+                y++;
+                currentPiece.setSouthNeighbor(nextSpace(x, y));
                 adjacentPiece = currentPiece.getSouthNeighbor();
+                System.out.println(adjacentPiece + "gotHere");
                 adjacentPiece.setNorthNeighbor(currentPiece);
 
             }
             if (r2 > .75)
             {
-
-                currentPiece.setWestNeighbor(new EmptySpace());
+                x--;
+                currentPiece.setWestNeighbor(nextSpace(x, y));
                 adjacentPiece = currentPiece.getWestNeighbor();
+                System.out.println(adjacentPiece + "gotHere");
                 adjacentPiece.setEastNeighbor(currentPiece);
             }
             currentPiece = adjacentPiece;
         }
+    }
+
+    private EmptySpace nextSpace(int x, int y)
+    {
+
+        if (mazePieceStore.containsKey(getXYString(x, y)))
+        {
+            System.out.println("get");
+            // return mazePieceStore.get(x + "," + y);
+            EmptySpace eS = mazePieceStore.get(getXYString(x, y));
+            System.out.println(eS);
+            System.out.println("hello");
+            return eS;
+
+        } else
+        {
+            System.out.println("put");
+            EmptySpace es = new EmptySpace();
+            mazePieceStore.put(getXYString(x, y), es);
+            return es;
+        }
+
+
+    }
+
+    private String getXYString(int x, int y)
+    {
+        return x + ", " + y;
+    }
+
+    private Integer getXY(int x, int y)
+    {
+        return new Integer(x * 1000 + y);
     }
 
     public MazePiece startingMazePiece()
